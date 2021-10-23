dcd9b80d8509818526487be9143a80d9eac2ed3f
Madis Pink <madis.pink@gmail.com>
2021-10-23 03:07:27 +0300

    fix: use legacy source/target compat in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,9 @@ gradlePlugin {
   }
 }
 
-tasks.compileJava {
-  options.release.set(8)
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
