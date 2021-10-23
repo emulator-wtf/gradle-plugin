f104ae6b68bc4e7b75ad8abbf8338211c9ea9fde
Madis Pink <mail@madis.pink>
2021-10-23 13:09:41 +0300

    chore: take S3 bucket from secrets

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,11 +36,12 @@ if (!pluginVersion.isNullOrBlank()) {
 // if aws creds & git tag is set, setup publishing
 val awsKey = System.getenv()["AWS_ACCESS_KEY_ID"]
 val awsSecret = System.getenv()["AWS_SECRET_ACCESS_KEY"]
+val awsBucket = System.getenv()["AWS_S3_BUCKET"]
 
-if (listOf(tag, awsKey, awsSecret).none { it.isNullOrBlank() }) {
+if (listOf(tag, awsKey, awsSecret, awsBucket).none { it.isNullOrBlank() }) {
   publishing {
     repositories {
-      maven("s3://***REMOVED***/releases/") {
+      maven("s3://${awsBucket}/releases/") {
         name = "s3"
         credentials(AwsCredentials::class.java) {
           accessKey = awsKey
