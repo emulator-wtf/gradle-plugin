e3e85dc2233f8b184b51a331d96617a5eb151243
Madis Pink <mail@madis.pink>
2023-04-26 21:09:25 +0300

    chore: remove old vanniktech maven publish from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,5 @@
 pluginManagement {
   includeBuild("build-logic")
-  resolutionStrategy {
-    eachPlugin {
-      if (requested.id.id == "com.vanniktech.maven.publish") {
-        useModule("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
-      }
-    }
-  }
   repositories {
     mavenCentral()
     maven("https://plugins.gradle.org/m2/")
