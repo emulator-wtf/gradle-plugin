4ed1b48986b5558ac587d20756759854abdd17e4
Madis Pink <1147723+madisp@users.noreply.github.com>
2023-05-31 01:58:10 +0300

    feat: pick up https.proxy* sys props in addition to http.proxy* (#12)

--- src/main/java/wtf/emulator/EwExtension.java
+++ src/main/java/wtf/emulator/EwExtension.java
@@ -8,7 +8,9 @@ import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 
 import javax.inject.Inject;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -38,10 +40,10 @@ public abstract class EwExtension implements EwInvokeConfiguration {
     getOutputs().convention(Collections.emptyList());
 
     // discover proxy settings from System properties
-    sysPropConvention(getProxyHost(), "http.proxyHost");
-    sysPropConvention(getProxyPort(), "http.proxyPort", Integer::parseInt);
-    sysPropConvention(getProxyUser(), "http.proxyUser");
-    sysPropConvention(getProxyPassword(), "http.proxyPassword");
+    sysPropConvention(getProxyHost(), "https.proxyHost", "http.proxyHost");
+    sysPropConvention(getProxyPort(), Arrays.asList("https.proxyPort", "http.proxyPort"), Integer::parseInt);
+    sysPropConvention(getProxyUser(), "https.proxyUser", "http.proxyUser");
+    sysPropConvention(getProxyPassword(), "https.proxyPassword", "http.proxyPassword");
 
     this.variantCount = objectFactory.property(Integer.class).convention(0);
   }
@@ -59,20 +61,24 @@ public abstract class EwExtension implements EwInvokeConfiguration {
     return this.variantCount;
   }
 
-  private static void sysPropConvention(Property<String> extProp, String key) {
-    sysPropConvention(extProp, key, identity());
+  private static void sysPropConvention(Property<String> extProp, String... keys) {
+    sysPropConvention(extProp, Arrays.asList(keys), identity());
   }
 
-  private static <T> void sysPropConvention(Property<T> extProp, String key, Function<String, T> transform) {
-    String sysPropValue = System.getProperty(key);
-    if (sysPropValue != null && !sysPropValue.isBlank()) {
-      try {
-        T transformed = transform.apply(sysPropValue);
-        if (transformed != null) {
-          extProp.convention(transformed);
+  private static <T> void sysPropConvention(Property<T> extProp, List<String> keys, Function<String, T> transform) {
+    for (String key : keys) {
+      String sysPropValue = System.getProperty(key);
+      if (sysPropValue != null && !sysPropValue.isBlank()) {
+        try {
+          T transformed = transform.apply(sysPropValue);
+          if (transformed != null) {
+            extProp.convention(transformed);
+            return;
+          }
         }
-      } catch (Exception e) {
-        // ignore transform failures, e.g. failing to parse an int
+        catch (Exception e) {
+          // ignore transform failures, e.g. failing to parse an int
+        }
       }
     }
   }
