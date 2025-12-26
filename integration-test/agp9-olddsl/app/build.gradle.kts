f87ca49fadd0c0a8f6c5b8e2d92006b8169d7d26
Madis Pink <mail@madis.pink>
2025-12-26 16:17:11 +0200

    fix: add a separate agp9-olddsl integration-test

---
+++ integration-test/agp9-olddsl/app/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import wtf.emulator.ewDevices
+import wtf.emulator.DeviceModel
+
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.baselineprofile)
+  alias(libs.plugins.integration.test.conventions)
+  alias(libs.plugins.emulatorwtf)
+}
+
+android {
+  kotlin.compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_11)
+  }
+
+  compileSdk = 36
+
+  defaultConfig {
+    applicationId = "wtf.emulator.sample"
+    minSdk = 23
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    debug {
+      enableAndroidTestCoverage = false
+    }
+  }
+
+  namespace = "wtf.emulator.sample"
+
+  testOptions.managedDevices.ewDevices {
+    register("ewPixel7api33") {
+      device = DeviceModel.PIXEL_7
+      apiLevel = 33
+    }
+  }
+}
+
+emulatorwtf {
+  targets {
+    testMethod("MinimalTests", "otherTest")
+  }
+}
+
+dependencies {
+  androidTestImplementation(libs.androidx.rules)
+  androidTestImplementation(libs.androidx.runner)
+  androidTestImplementation(libs.androidx.core)
+  androidTestImplementation(libs.androidx.junit.ktx)
+  "baselineProfile"(project(":baselineprofile"))
+}
---
+++ integration-test/agp9-olddsl/app/src/androidTest/java/MinimalTests.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MinimalTests {
+  @Test
+  public void junitWorks() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void otherTest() {
+    assertTrue(true);
+  }
+}
